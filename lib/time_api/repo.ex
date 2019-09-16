5791442a9a11d12b016509976e3546dd611b2bb6
natdev <dordonne.nathanael@gmail.com>
2019-09-16 08:46:29 -0400

    docker

--- lib/time_api/repo.ex
+++ lib/time_api/repo.ex
@@ -3,3 +3,18 @@ defmodule Timeapi.Repo do
     otp_app: :time_api,
     adapter: Ecto.Adapters.Postgres
 end
+defmodule Timeapi.Repo do
+  use Ecto.Repo, 
+  otp_app: :time_api,
+  adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+    config = config
+      |> Keyword.put(:username, System.get_env("PGUSER"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST"))
+      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+    {:ok, config}
+  end
+end
